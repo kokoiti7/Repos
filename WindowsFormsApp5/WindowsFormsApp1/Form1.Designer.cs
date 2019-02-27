f83a1dd324fef605850f648329c6de66c7b135e4
tanizaki <tanizakikota1120@gmai.com>
2019-02-27 12:22:18 +0900

    Revert "update download delete 追加"
    This reverts commit d96e0ba44840cea1f4f8bbe5d2895541e7c41050.

--- WindowsFormsApp5/WindowsFormsApp1/Form1.Designer.cs
+++ WindowsFormsApp5/WindowsFormsApp1/Form1.Designer.cs
@@ -28,19 +28,87 @@
         /// </summary>
         private void InitializeComponent()
         {
+            this.textBox1 = new System.Windows.Forms.TextBox();
+            this.webBrowser1 = new System.Windows.Forms.WebBrowser();
+            this.button1 = new System.Windows.Forms.Button();
+            this.webBrowser2 = new System.Windows.Forms.WebBrowser();
+            this.button2 = new System.Windows.Forms.Button();
+            this.button3 = new System.Windows.Forms.Button();
+            this.textBox2 = new System.Windows.Forms.TextBox();
             this.button4 = new System.Windows.Forms.Button();
             this.button5 = new System.Windows.Forms.Button();
             this.listBox1 = new System.Windows.Forms.ListBox();
             this.button6 = new System.Windows.Forms.Button();
-            this.button1 = new System.Windows.Forms.Button();
             this.SuspendLayout();
+            // 
+            // textBox1
+            // 
+            this.textBox1.Location = new System.Drawing.Point(12, 52);
+            this.textBox1.Name = "textBox1";
+            this.textBox1.Size = new System.Drawing.Size(337, 19);
+            this.textBox1.TabIndex = 0;
+            this.textBox1.Leave += new System.EventHandler(this.textBox1_Leave);
+            // 
+            // webBrowser1
+            // 
+            this.webBrowser1.Location = new System.Drawing.Point(686, 176);
+            this.webBrowser1.MinimumSize = new System.Drawing.Size(20, 20);
+            this.webBrowser1.Name = "webBrowser1";
+            this.webBrowser1.Size = new System.Drawing.Size(342, 250);
+            this.webBrowser1.TabIndex = 1;
+            this.webBrowser1.Url = new System.Uri("", System.UriKind.Relative);
+            // 
+            // button1
+            // 
+            this.button1.Location = new System.Drawing.Point(894, 127);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(134, 23);
+            this.button1.TabIndex = 2;
+            this.button1.Text = "button1";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // webBrowser2
+            // 
+            this.webBrowser2.Location = new System.Drawing.Point(12, 176);
+            this.webBrowser2.MinimumSize = new System.Drawing.Size(20, 20);
+            this.webBrowser2.Name = "webBrowser2";
+            this.webBrowser2.Size = new System.Drawing.Size(337, 250);
+            this.webBrowser2.TabIndex = 3;
+            this.webBrowser2.Url = new System.Uri("", System.UriKind.Relative);
+            // 
+            // button2
+            // 
+            this.button2.Location = new System.Drawing.Point(215, 127);
+            this.button2.Name = "button2";
+            this.button2.Size = new System.Drawing.Size(134, 23);
+            this.button2.TabIndex = 4;
+            this.button2.Text = "button2";
+            this.button2.UseVisualStyleBackColor = true;
+            this.button2.Click += new System.EventHandler(this.button2_Click);
+            // 
+            // button3
+            // 
+            this.button3.Location = new System.Drawing.Point(686, 127);
+            this.button3.Name = "button3";
+            this.button3.Size = new System.Drawing.Size(134, 23);
+            this.button3.TabIndex = 5;
+            this.button3.Text = "button3";
+            this.button3.UseVisualStyleBackColor = true;
+            this.button3.Click += new System.EventHandler(this.button3_Click);
+            // 
+            // textBox2
+            // 
+            this.textBox2.Location = new System.Drawing.Point(686, 51);
+            this.textBox2.Name = "textBox2";
+            this.textBox2.Size = new System.Drawing.Size(342, 19);
+            this.textBox2.TabIndex = 6;
             // 
             // button4
             // 
-            this.button4.Font = new System.Drawing.Font("メイリオ", 11.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(128)));
-            this.button4.Location = new System.Drawing.Point(30, 37);
+            this.button4.Location = new System.Drawing.Point(529, 431);
             this.button4.Name = "button4";
-            this.button4.Size = new System.Drawing.Size(103, 37);
+            this.button4.Size = new System.Drawing.Size(134, 23);
             this.button4.TabIndex = 7;
             this.button4.Text = "upload";
             this.button4.UseVisualStyleBackColor = true;
@@ -48,10 +116,9 @@
             // 
             // button5
             // 
-            this.button5.Font = new System.Drawing.Font("メイリオ", 11.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(128)));
-            this.button5.Location = new System.Drawing.Point(162, 37);
+            this.button5.Location = new System.Drawing.Point(529, 377);
             this.button5.Name = "button5";
-            this.button5.Size = new System.Drawing.Size(103, 37);
+            this.button5.Size = new System.Drawing.Size(134, 23);
             this.button5.TabIndex = 8;
             this.button5.Text = "download";
             this.button5.UseVisualStyleBackColor = true;
@@ -61,56 +128,59 @@
             // 
             this.listBox1.FormattingEnabled = true;
             this.listBox1.ItemHeight = 12;
-            this.listBox1.Location = new System.Drawing.Point(30, 140);
+            this.listBox1.Location = new System.Drawing.Point(357, 127);
             this.listBox1.Name = "listBox1";
-            this.listBox1.Size = new System.Drawing.Size(358, 424);
+            this.listBox1.Size = new System.Drawing.Size(306, 232);
             this.listBox1.TabIndex = 9;
             this.listBox1.SelectedIndexChanged += new System.EventHandler(this.listBox1_SelectedIndexChanged);
             // 
             // button6
             // 
-            this.button6.Font = new System.Drawing.Font("メイリオ", 11.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(128)));
-            this.button6.Location = new System.Drawing.Point(285, 37);
+            this.button6.Location = new System.Drawing.Point(357, 431);
             this.button6.Name = "button6";
-            this.button6.Size = new System.Drawing.Size(103, 37);
+            this.button6.Size = new System.Drawing.Size(134, 23);
             this.button6.TabIndex = 10;
             this.button6.Text = "delete";
             this.button6.UseVisualStyleBackColor = true;
             this.button6.Click += new System.EventHandler(this.button6_Click);
             // 
-            // button1
-            // 
-            this.button1.Location = new System.Drawing.Point(285, 97);
-            this.button1.Name = "button1";
-            this.button1.Size = new System.Drawing.Size(103, 37);
-            this.button1.TabIndex = 11;
-            this.button1.Text = "button1";
-            this.button1.UseVisualStyleBackColor = true;
-            this.button1.Click += new System.EventHandler(this.button1_Click);
-            // 
             // Form1
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 12F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(1022, 613);
-            this.Controls.Add(this.button1);
+            this.ClientSize = new System.Drawing.Size(1040, 613);
             this.Controls.Add(this.button6);
             this.Controls.Add(this.listBox1);
             this.Controls.Add(this.button5);
             this.Controls.Add(this.button4);
+            this.Controls.Add(this.textBox2);
+            this.Controls.Add(this.button3);
+            this.Controls.Add(this.button2);
+            this.Controls.Add(this.webBrowser2);
+            this.Controls.Add(this.button1);
+            this.Controls.Add(this.webBrowser1);
+            this.Controls.Add(this.textBox1);
             this.Name = "Form1";
             this.Text = "Form1";
             this.Load += new System.EventHandler(this.Form1_Load);
             this.ResumeLayout(false);
+            this.PerformLayout();
 
         }
 
         #endregion
+
+        private System.Windows.Forms.TextBox textBox1;
+        private System.Windows.Forms.WebBrowser webBrowser1;
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.WebBrowser webBrowser2;
+        private System.Windows.Forms.Button button2;
+        private System.Windows.Forms.Button button3;
+        private System.Windows.Forms.TextBox textBox2;
         private System.Windows.Forms.Button button4;
         private System.Windows.Forms.Button button5;
         private System.Windows.Forms.ListBox listBox1;
         private System.Windows.Forms.Button button6;
-        private System.Windows.Forms.Button button1;
     }
 }
 
